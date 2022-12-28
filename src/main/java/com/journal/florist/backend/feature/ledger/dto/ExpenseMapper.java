d0fa0e47ee65d96802e1aaa9deae1ffd466255a1
Rizky Jabar <rkhamet12@gmail.com>
2022-12-28 22:02:59 +0700

    27/12/2022 | New: - Add new expense mapper

---
+++ src/main/java/com/journal/florist/backend/feature/ledger/dto/ExpenseMapper.java
@@ -0,0 +1,58 @@
+package com.journal.florist.backend.feature.ledger.dto;
+
+import com.journal.florist.app.common.utils.converter.DateConverter;
+import com.journal.florist.backend.feature.ledger.model.Expense;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Component
+public class ExpenseMapper implements Serializable {
+    private String expenseId;
+    private String date;
+    private String createdBy;
+    private String additionalInfo;
+    private BigDecimal amount;
+    private String payFor;
+    private String lastModifiedBy;
+    private String lastModifiedDate;
+
+    public ExpenseMapper buildExpenseResponse(Expense expense) {
+
+        var dateTime = DateConverter.toLocalDateTime(expense.getCreatedAt());
+        String addedDate = DateConverter.formatDate().format(dateTime);
+
+        String modifiedDate = null;
+
+        String modifiedBy = expense.getLastModifiedBy();
+
+        if (expense.getLastModifiedDate() != null) {
+            var toLocalDate = DateConverter.toLocalDateTime(expense.getLastModifiedDate());
+            modifiedDate = DateConverter.formatDate().format(toLocalDate);
+        }
+
+        if (modifiedBy == null) {
+            lastModifiedBy = null;
+        }
+
+        return ExpenseMapper.builder()
+                .expenseId(expense.getPublicKey())
+                .date(addedDate)
+                .createdBy(expense.getCreatedBy())
+                .additionalInfo(expense.getAdditionalInformation())
+                .amount(expense.getAmount())
+                .payFor(expense.getPayFor().getName())
+                .lastModifiedBy(lastModifiedBy)
+                .lastModifiedDate(modifiedDate)
+                .build();
+    }
+}
